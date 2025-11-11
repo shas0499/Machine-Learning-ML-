2a6fe1dbd5f2a4b6f3f3bd00473a1f4ddef2e33e
Shaswata <shaswata.barua0499@outlook.com>
2025-11-11 12:25:25 +0530

    Add files via upload

---
+++ Python Basic/Lambda Function/Lambda.py
@@ -0,0 +1,9 @@
+# lambda arguments : expression
+add = lambda x,y : x+y
+print(add(20,30))
+
+sub = lambda a,b : a-b
+print(f"Subtraction : {sub(30,10)}")
+
+sqr = lambda i : i**2
+print(f"Square : {sqr(10)}")
